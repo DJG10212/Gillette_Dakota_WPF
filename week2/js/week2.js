04a14146ca8d9b2ed40cb3c538108734d5cefcb5
Dakota Gillette <dgillette@fullsail.edu>
2014-10-08 15:19:54 -0700

    Asked the user for name and year born

--- week2/js/week2.js
+++ week2/js/week2.js
@@ -5,6 +5,19 @@ Assignment: Expressions
 10-8-2014
 */
 
-alert("testing 1, 2, 3,");
+//ask the user their name
+var name = prompt("Please type in your name:");
+
+
+//Say hello to the user
+alert ("Welcome "+name+"! Lets go ahead and figure out how old you are!");
+
+
+
+
+var yearBorn = prompt("What year were you born?");
+
+
+console.log (yearBorn);
 
 
