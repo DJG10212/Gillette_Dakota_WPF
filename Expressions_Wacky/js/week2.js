d9501fcda2fb9630e92c50f7101c8df0d05c63af
Dakota Gillette <dgillette@fullsail.edu>
2014-10-09 19:40:38 -0700

    finished questions. Now on to calculations

--- Expressions_Wacky/js/week2.js
+++ Expressions_Wacky/js/week2.js
@@ -13,6 +13,20 @@ alert ("How many times a day does your neighbors dog bark?")
 //ask user how many times per day the dog barks?
 var mon = parseInt(prompt("How many times did the dog bark on Monday?"))
 
+var tue = parseInt(prompt("How many times did the dog bark on Tuesday?"))
+
+var wed = parseInt(prompt("How many times did the dog bark on Wednesday?"))
+
+var thur = parseInt(prompt("How many times did the dog bark on Thursday?"))
+
+var fri = parseInt(prompt("How many times did the dog bark on Friday?"))
+
+var sat = parseInt(prompt("How many times did the dog bark on Saturday?"))
+
+var sun = parseInt(prompt("How many times did the dog bark on Sunday?"))
+
+
+
 
 
 
