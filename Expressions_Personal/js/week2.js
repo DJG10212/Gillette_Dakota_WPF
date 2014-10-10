cc858dac361b1867e05ac2f1d5ad2e399285ee07
Dakota Gillette <dgillette@fullsail.edu>
2014-10-09 18:15:16 -0700

    starting

---
+++ Expressions_Personal/js/week2.js
@@ -0,0 +1,11 @@
+/*
+Dakota Gillette
+WPF Section 01
+Assignment: Expressions Personal
+10-8-2014
+*/
+
+//calculate your current grade in the class
+
+alert ("this will calculate your average grade percentage based off of 5 assignments all weighted equally")
+
