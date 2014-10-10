3f5f998aa85f5688fe9c607ad24584a1cf970c8e
Dakota Gillette <dgillette@fullsail.edu>
2014-10-09 17:39:51 -0700

    added days worked prompts

---
+++ Expressions_Industry/js/week2.js
@@ -0,0 +1,24 @@
+/*
+Dakota Gillette
+WPF Section 01
+Assignment: Expressions Industry
+10-8-2014
+*/
+
+
+//Calculate the total hours worked, and how much money was earned
+
+//ask the user what their hourly rate of pay is
+
+var hourlyRate = prompt("What is your hourly rate of pay? (example: 10.19)")
+
+//ask user how many hours they worked each day
+var monday = prompt("How many hours did you work on Monday? (example: 9)")
+
+var tuesday = prompt("How many hours did you work on Monday? (example: 9)")
+
+var wednesday = prompt("How many hours did you work on Monday? (example: 9)")
+
+var thursday = prompt("How many hours did you work on Monday? (example: 9)")
+
+var friday = prompt("How many hours did you work on Monday? (example: 9)")
