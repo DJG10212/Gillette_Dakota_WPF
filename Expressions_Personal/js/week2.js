39c230de3a939e67963cae5c0c47746cbb5af84e
Dakota Gillette <dgillette@fullsail.edu>
2014-10-09 18:22:07 -0700

    added average calculation

--- Expressions_Personal/js/week2.js
+++ Expressions_Personal/js/week2.js
@@ -9,15 +9,33 @@ Assignment: Expressions Personal
 
 alert ("this will calculate your average grade percentage based off of 5 assignments all weighted equally")
 
+//ask user each grade percentage for 5 assignments
+var test1 = parseInt(prompt("What was your grade percentage on the first assignment?"))
 
-var test1 = parseInt(prompt("What was your grade on the first assignment?"))
+var test2 = parseInt(prompt("What was your grade percentage on the second assignment?"))
 
-var test2 = parseInt(prompt("What was your grade on the second assignment?"))
+var test3 = parseInt(prompt("What was your grade percentage on the third assignment?"))
 
-var test3 = parseInt(prompt("What was your grade on the third assignment?"))
+var test4 = parseInt(prompt("What was your grade percentage on the fourth assignment?"))
 
-var test4 = parseInt(prompt("What was your grade on the fourth assignment?"))
+var test5 = parseInt(prompt("What was your grade percentage on the fifth assignment?"))
 
-var test5 = parseInt(prompt("What was your grade on the fifth assignment?"))
+
+//add up total
+var testTotal = test1+test2+test3+test4+test5;
+
+//calculate the average score
+
+var averageScore = testTotal / 5
+
+
+
+
+
+
+
+
+
+
 
 
