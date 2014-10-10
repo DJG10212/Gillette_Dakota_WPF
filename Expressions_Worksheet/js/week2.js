3925cc03159a3e6aa7f3618b174bc06e355a2cfa
Dakota Gillette <dgillette@fullsail.edu>
2014-10-09 17:02:12 -0700

    Worksheet

---
+++ Expressions_Worksheet/js/week2.js
@@ -0,0 +1,126 @@
+/*
+Dakota Gillette
+WPF Section 01
+Assignment: Expressions Worksheet
+10-9-2014
+*/
+
+
+
+//Calculate the area of a rectangle
+
+//the width
+var width = 10;
+
+//the height
+var height =7;
+
+//multiply height and width to get the area
+var area = height * width
+
+//print the results
+console.log ("The area of the rectangle is " + area)
+
+
+
+
+
+//Problem: Dog Years
+
+//Sparky's age:
+
+var age = 5;
+
+//calculate sparky's age in "dog years"
+
+var dogYears = 7 * age;
+
+//print the results
+console.log ("Sparky is " + age + " human years old which is " + dogYears + " in dog years.")
+
+
+
+
+
+
+
+
+//Problem: Slice of Pie part I
+
+
+//number of slices per pizza
+var slicePizza = 8;
+
+
+//number of people at the party
+var totalPeople = 10;
+
+
+//number of pizzas ordered
+var totalPizza = 4;
+
+
+
+//calculate the number of slices per person
+var totalEaten = slicePizza * totalPizza / totalPeople;
+
+
+//print results
+console.log ("Each person ate " +totalEaten+ " slices of pizza at the party.")
+
+
+
+
+
+//Problem: Slice of Pie part II
+
+
+//calculate the number of slices that Sparky gets
+
+var sparky = totalPeople % totalPizza;
+
+
+//printResults
+console.log ("Sparky got " +sparky+ " slices of pizza.")
+
+
+
+
+
+
+//Problem: Average shopping bill
+
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
