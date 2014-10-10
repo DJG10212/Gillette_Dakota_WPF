2db879ccf14edd7d216d5620a9852e8b6224ceaf
Dakota Gillette <dgillette@fullsail.edu>
2014-10-09 18:17:54 -0700

    added all user prompts

--- Expressions_Personal/js/week2.js
+++ Expressions_Personal/js/week2.js
@@ -9,3 +9,15 @@ Assignment: Expressions Personal
 
 alert ("this will calculate your average grade percentage based off of 5 assignments all weighted equally")
 
+
+var test1 = parseInt(prompt("What was your grade on the first assignment?"))
+
+var test2 = parseInt(prompt("What was your grade on the second assignment?"))
+
+var test3 = parseInt(prompt("What was your grade on the third assignment?"))
+
+var test4 = parseInt(prompt("What was your grade on the fourth assignment?"))
+
+var test5 = parseInt(prompt("What was your grade on the fifth assignment?"))
+
+
