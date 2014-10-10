3a193060118907e1d9c2c53d80f0dfc0b7a594ae
Dakota Gillette <dgillette@fullsail.edu>
2014-10-09 17:59:18 -0700

    total hours worked calculation works

--- Expressions_Industry/js/week2.js
+++ Expressions_Industry/js/week2.js
@@ -11,19 +11,26 @@ Assignment: Expressions Industry
 //ask the user what their hourly rate of pay is
 
 var hourlyRate = prompt("What is your hourly rate of pay? (example: 10.19)")
+var hourlyRate = parseInt(hourlyRate)
 
 //ask user how many hours they worked each day
 var monday = prompt("How many hours did you work on Monday? (example: 9)")
+var monday = parseInt(monday)
 
-var tuesday = prompt("How many hours did you work on Monday? (example: 9)")
+var tuesday = prompt("How many hours did you work on Tuesday? (example: 9)")
+tuesday = parseInt(tuesday)
 
-var wednesday = prompt("How many hours did you work on Monday? (example: 9)")
+var wednesday = prompt("How many hours did you work on Wednesday? (example: 9)")
+wednesday = parseInt(wednesday)
 
-var thursday = prompt("How many hours did you work on Monday? (example: 9)")
+var thursday = prompt("How many hours did you work on Thursday? (example: 9)")
+thursday = parseInt(thursday)
 
-var friday = prompt("How many hours did you work on Monday? (example: 9)")
+var friday = prompt("How many hours did you work on Friday? (example: 9)")
+friday = parseInt(friday)
+
 
 //calculate total hours worked
-var totalHours = monday+tuesday+wednesday+thursday+friday
+var totalHours = monday + tuesday + wednesday + thursday + friday;
 
-console
+var moneyEarned = totalHours * hourlyRate
